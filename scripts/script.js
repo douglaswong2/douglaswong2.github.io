0e4aea745f03d393d60514b05f0689321877cfcf
douglaswong2 <33472480+douglaswong2@users.noreply.github.com>
2017-11-21 22:20:25 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,17 @@ $(document).ready(function(){
             if(statusTxt == "error")
                 $("#page__content").text("Error: " + xhr.status + ": " + xhr.statusText);
         });
+        $("#page__title").text("Who am I?");
     });    
     $("#link_photos").click(function(){
         $("#page__content").load("photos.txt", function(responseTxt, statusTxt, xhr){
             if(statusTxt == "error")
                 $("#page__content").text("Error: " + xhr.status + ": " + xhr.statusText);
-        });
+        });        
+        $("#page__title").text("Photos");
+    });  
+    $("#link_img1").click(function(){
+        $("#page__content").text('<img src="https://www.catprotection.com.au/wp-content/uploads/2014/11/5507692-cat-m.jpg" alt="" />');
+        $("#page__title").text("Little Cat");
     });
 });
